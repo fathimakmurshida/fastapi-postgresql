e4076a12898513779de28c5b1f43ebaa6c6cae1a
fathimakmurshida <fathimakmurshida@gmail.com>
2022-04-18 16:21:12 +0530

    fix

---
+++ models.py
@@ -0,0 +1,13 @@
+from enum import unique
+from database import Base
+
+from sqlalchemy import String,Boolean,Integer,Column
+
+class Register(Base):
+    __tablename__='user'
+    id=Column(Integer,primary_key=True)
+    name=Column(String(255),nullable=False)
+    phone=Column(String(255),nullable=False,unique=True)
+ 
+    def __repr__(self):
+        return f"<user_register name={self.name} phone={self.phone}>"              
